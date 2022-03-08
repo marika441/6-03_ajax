78cb7e85d878f39d31676c64cd949b558b86e513
marika441 <92457238+marika441@users.noreply.github.com>
2022-03-08 23:48:56 +0900

    [Finish]6-3AjaxとJSON

--- script.js
+++ script.js
@@ -5,10 +5,22 @@ $(document).ready(function() {
     $.ajax({url: 'data.json', dataType: 'json'})
     .done(function(data) {
         data.forEach(function(item, index) {
-            console.log(item);
+          if(item.crowded === 'yes') {
+            const idName = '#' + item.id;
+            $(idName).find('.check').addClass('crowded');
+          }
+            // console.log(item);
         });
     })
     .fail(function() {
         window.alert('読み込みエラー');
     });
+  // クリックされたら空き席状況を表示
+  $('.check').on('click', function(){
+    if($(this).hasClass('crowded')) {
+      $(this).text('残席わずか').addClass('red');
+    } else {
+      $(this).text('お席あります').addClass('green');
+    }
+  });
 });
